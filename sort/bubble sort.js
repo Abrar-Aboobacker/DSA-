bbf6b341b824fdbdf7c898ae5ed97b7fcb807a38
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-05-17 19:59:41 +0530

    studied bubble and insertion sort algorithm

---
+++ sort/bubble sort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr){
+    let swapped
+  do{
+    swapped = false
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]>arr[i+1]){
+            let temp = arr[i]
+            arr[i]=arr[i+1]
+            arr[i+1]=temp
+            swapped=true
+        }
+    }
+  }while(swapped)
+}
+
+const arr = [1,5,4,9,3]
+bubbleSort(arr)
+console.log(arr);
+
+// Big-O O(n^2)
